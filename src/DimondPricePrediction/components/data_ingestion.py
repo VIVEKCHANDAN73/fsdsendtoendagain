82ff2626d45e65e9771b465a020bef040002d536
Vivek Chandan <vivekchandan73@gmail.com>
2024-09-06 09:02:31 +0530

    data tranformation completed

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -39,9 +39,13 @@ class DataIngestion:
 
             test_data.to_csv(self.ingestion_config.test_data_path,index=False)
             logging.info('I have saved the test dataset in artifacts folder')
-            
+
             logging.info("train test split completed")
 
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
         except Exception as e:
             logging.info('Exception occured during data ingestion stage')
             raise customexception(e,sys)
